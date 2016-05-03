fe885ce6e7960beb4b70070deb04ba608264c2f9
Lin <xxice520@hotmail.com>
2016-05-03 19:56:36 +0800

    new PhoneBook & Adapter

---
+++ app/src/main/java/main/MainActivity.java
@@ -0,0 +1,38 @@
+package main;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.user.www.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvPhone = (ListView) findViewById(R.id.listPhone);
+
+        List<PhoneBook> listPhoneBook = new ArrayList<PhoneBook>();
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.mipmap.pic_1),
+                "Pete Houston", "010-9817-6331"));
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.mipmap.pic_2),
+                "Lina Cheng", "046-7764-1142"));
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.mipmap.pic_3),
+                "Jenny Nguyen", "0913-223-498"));
+        PhoneBookAdapter adapter = new PhoneBookAdapter(this, listPhoneBook);
+        lvPhone.setAdapter(adapter);
+    }
+}
+
