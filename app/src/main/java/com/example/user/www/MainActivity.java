75a57326f29f2b15d3a0b27b9e7b2fbc69f030b8
Swc <snoopylaughat101@gmail.com>
2016-05-17 16:44:24 +0800

    add the map activity and layout and activate the google API.

--- app/src/main/java/com/example/user/www/MainActivity.java
+++ app/src/main/java/com/example/user/www/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button call_btn;
+    private Button show_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent call_intent = new Intent(MainActivity.this, CallActivity.class);
+                startActivity(call_intent);
+            }
+        });
+
+        show_btn = (Button) findViewById(R.id.showmapbutton);
+        show_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent call_intent = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(call_intent);
             }
         });
